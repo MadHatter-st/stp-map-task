4071bd444cf8841a4d1e9d17b18f9782be427ae6
Rainbow49 <n_khmelev@mail.ru>
2022-05-14 23:43:13 +0900

    Add map collection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
+    public static void main(String[] args) throws IOException {//commit
+        Map<Integer, String> map = new TreeMap<Integer, String>();
+        BufferedReader in = new BufferedReader(new FileReader("access.log"));
+        String  line = "";
+        int i = 0;
+        while ((line = in.readLine()) != null) {
+            String[] parts = line.split("\\s+");
+            i++;
+            map.put(i, parts[2] + " " + parts[4]);
+        }
+        in.close();
+        System.out.println(map.toString());
+    }
 }
