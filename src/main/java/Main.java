052f2719a2250a348e6a32c318aa36f70e899f41
Rainbow49 <n_khmelev@mail.ru>
2022-05-15 12:43:08 +0900

    add map collection with max traffic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,56 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException {//commit
+    public static void main(String[] args) throws IOException {
+
         Map<Integer, String> map = new TreeMap<Integer, String>();
-        Map<String,Integer> test = new TreeMap<String,Integer>();
+        Map<Integer, String> logs = new TreeMap<Integer, String>();
+        Map<String, Double> data = new HashMap<String, Double>();
+        ValueComparator bvc = new ValueComparator(data);
+        Map<String, Integer> time = new TreeMap<String, Integer>();
+        Map<String, Double> maxData = new TreeMap<String, Double>(bvc);
         BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\User\\Desktop\\access.log"));
-        String  line = "";
+        String line = "";
         int i = 0;
         while ((line = in.readLine()) != null) {
             String[] parts = line.split("\\s+");
             i++;
             map.put(i, parts[2]);
-            if(!test.containsKey(parts[2])){
-                test.put(parts[2],Integer.parseInt(parts[4]));
-            }else{
-                test.put(parts[2], test.get(parts[2]) + Integer.parseInt(parts[4]));
+            logs.put(i, line);
+            if (!data.containsKey(parts[2])) {
+                data.put(parts[2], Double.parseDouble(parts[4]));
+                time.put(parts[2], Integer.parseInt(parts[1]));
+            } else {
+                data.put(parts[2], data.get(parts[2]) + Double.parseDouble(parts[4]));
+                time.put(parts[2], time.get(parts[2]) + Integer.parseInt(parts[1]));
             }
+            data.put(parts[2], data.get(parts[2]) / time.get(parts[2]));
         }
         in.close();
-        System.out.println(test.toString());
+
+        maxData.putAll(data);
+        System.out.println("results: " + maxData);
+
+//        System.out.println(data.toString());
+//        data.entrySet().stream()
+//                .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+//                .forEach();
+
+        try (FileWriter writer = new FileWriter("out.txt")) {
+                for (Integer key : logs.keySet()) {
+                    if(maxData.containsKey(map.get(key))){
+                        writer.write(logs.get(key)+"\n");
+                    }
+                }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
