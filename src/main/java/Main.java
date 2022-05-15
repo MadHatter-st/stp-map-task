6a6a4917e5effabe1332e57b1219e79124726804
Rainbow49 <n_khmelev@mail.ru>
2022-05-15 09:36:32 +0900

    add test map with IP client and their used bytes of data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,15 +7,21 @@ import java.util.TreeMap;
 public class Main {
     public static void main(String[] args) throws IOException {//commit
         Map<Integer, String> map = new TreeMap<Integer, String>();
+        Map<String,Integer> test = new TreeMap<String,Integer>();
         BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\User\\Desktop\\access.log"));
         String  line = "";
         int i = 0;
         while ((line = in.readLine()) != null) {
             String[] parts = line.split("\\s+");
             i++;
-            map.put(i, parts[2] + " " + parts[4]);
+            map.put(i, parts[2]);
+            if(!test.containsKey(parts[2])){
+                test.put(parts[2],Integer.parseInt(parts[4]));
+            }else{
+                test.put(parts[2], test.get(parts[2]) + Integer.parseInt(parts[4]));
+            }
         }
         in.close();
-        System.out.println(map.toString());
+        System.out.println(test.toString());
     }
 }
